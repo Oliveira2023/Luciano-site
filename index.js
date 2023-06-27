c67f4f9365e911c5165de00e702f56c2edd1b9f9
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2023-06-26 23:44:12 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let menuEl = document.getElementById("menu-principal")
+function menuClick(){
+    if (menuEl.style.display == "block"){
+        menuEl.style.display = "none"
+    }else{
+        menuEl.style.display = "block"
+    }
+ 
+}
