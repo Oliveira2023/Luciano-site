cad66753314fdc3751d65da8f2ddff0847e406a7
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2023-06-28 21:08:44 -0300

    atualização280623

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let menuEl = document.getElementById("menu-principal")
+let lang = document.querySelector("#lang")
 function menuClick(){
     if (menuEl.style.display == "block"){
         menuEl.style.display = "none"
     }else{
         menuEl.style.display = "block"
     }
- 
 }
+lang.addEventListener('click', ()=>{
+    lang.innerHTML = 'EN'
+})
