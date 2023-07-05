d7fcac8e7214b6b3dfe7608b0fa682f424ddb4a3
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2023-07-05 00:40:47 -0300

    anima fotos
    fotos aparecendo temporizadas

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let menuEl = document.getElementById("menu-principal")
 let lang = document.querySelector("#lang")
+let galeryBoton = document.getElementById("galeryboton")
+let baloes = document.querySelector('#baloes')
+let vale = document.querySelector('#vale')
+let penhasco = document.querySelector('#penhasco')
+let rio = document.querySelector('#rio')
+let cassete = document.querySelector('#cassete')
+
 function menuClick(){
     if (menuEl.style.display == "block"){
         menuEl.style.display = "none"
@@ -9,4 +16,38 @@ function menuClick(){
 }
 lang.addEventListener('click', ()=>{
     lang.innerHTML = 'EN'
+})
+galeryBoton.addEventListener('click', ()=>{
+    baloes.style.display = "block"
+    galeryBoton.style.display = 'none'
+    setTimeout(() => {
+        baloes.style.display = "none"
+        rio.style.display = 'block'
+        //galeryBoton.style.display = 'block'
+        setTimeout(() => {
+            rio.style.display = 'none'
+            vale.style.display = 'block'
+            
+            setTimeout(() => {
+                vale.style.display = 'none'
+                cassete.style.display = 'block'
+                
+                setTimeout(() => {
+                    cassete.style.display = 'none'
+                    penhasco.style.display = 'block'
+                    setTimeout(() => {
+                        penhasco.style.display = 'none'
+                        galeryBoton.style.display = 'block'
+                    }, 2000);
+                }, 2000);
+            }, 2000);
+        }, 2000);
+    }, 2000);
+    
+    
+    
+})
+baloes.addEventListener('click', ()=>{
+    baloes.style.display = "none"
+    galeryBoton.style.display = 'block'
 })
